5c70ec7ded6b321c3fa0681b78e979561be14cd9
Sam Le <samsonle@live.com>
2015-11-22 14:55:07 +0100

    Added Iterator to ArrayList fixes

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -14,8 +14,7 @@ public class ArrayList<E> implements List<E> {
         private boolean removable = false;
 
 
-        /**
-         * Test if there are more elements in list
+        /** Test if there are more elements in list
          *
          * @return true if more item in list
          */
@@ -26,7 +25,6 @@ public class ArrayList<E> implements List<E> {
 
         /**
          * Return the next object in the iterator
-         *
          * @return next object
          * @throws NoSuchElementException
          */
@@ -40,7 +38,6 @@ public class ArrayList<E> implements List<E> {
 
         /**
          * Removes the element returned by most recent call to next
-         *
          * @throws IllegalStateException if next has not yet been called
          * @throws IllegalStateException if remove was already called since recent next
          */
@@ -111,50 +108,7 @@ public class ArrayList<E> implements List<E> {
         return temp;
     }
 
-    private class ArrayIterator implements Iterator<E> {
-        private int j = 0;
-        private boolean removable = false;
-
-
-        /**
-         * Test if there are more elements in list
-         *
-         * @return true if more item in list
-         */
-        @Override
-        public boolean hasNext() {
-            return j < size;
-        }
-
-        /**
-         * Return the next object in the iterator
-         *
-         * @return next object
-         * @throws NoSuchElementException
-         */
-        @Override
-        public E next() throws NoSuchElementException {
-            if (j == size)
-                throw new NoSuchElementException("No next element");
-            removable = true;
-            return data[j++];
-        }
-
-        /**
-         * Removes the element returned by most recent call to next
-         *
-         * @throws IllegalStateException if next has not yet been called
-         * @throws IllegalStateException if remove was already called since recent next
-         */
-        @Override
-        public void remove() throws IllegalStateException {
-            if (!removable)
-                throw new IllegalStateException("nothing to remove");
-            ArrayList.this.remove(j - 1);
-            j--;
-            removable = false;
-        }
-    }    private void resize(int capacity) {
+   private void resize(int capacity) {
         E[] temp = (E[]) new Object[capacity];
         for (int i = 0; i < size; i++)
             temp[i] = data[i];
